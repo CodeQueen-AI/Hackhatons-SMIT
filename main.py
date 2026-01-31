c090a2998848a304f6a127184475164600e8836a
CodeQueen-AI <codeq209@gmail.com>
2026-01-31 11:17:21 +0500

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+# from statistics import mean, median, mode
+
+
+# # df = pd.read_csv("student_performance.csv")
+# # clean_df, insights = full_eda(df, target="exam_score")
+
+
+# print("Mean:", mean(numbers))
+# print("Median:", median(numbers))
+# print("Mode:", mode(numbers))
---
+++ statistics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Mean Function
+def mean(data):
+    return np.mean(data)
+
+# Median Function
+def median(data):
+    return np.median(data)
+
+# Mode Function
+def mode(data):
+    return max(set(data), key=data.count)
