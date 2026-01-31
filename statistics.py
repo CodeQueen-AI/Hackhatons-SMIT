c51fb019b22508f69a018a2b1b46826f13ea52fd
CodeQueen-AI <codeq209@gmail.com>
2026-01-31 11:42:42 +0500

    update

--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 # Mean Function
 def mean(data):
@@ -19,4 +20,23 @@ def variance(data):
 # Standard Deviation
 def standard_deviation(data):
     return np.std(data, ddof=1)
+
+# Z-Test
+def z_test(sample, population_mean):
+    sample_mean = np.mean(sample)
+    sample_std = np.std(sample, ddof=1)
+    n = len(sample)
+
+    z = (sample_mean - population_mean) / (sample_std / np.sqrt(n))
+    return 
+
+# T-Test
+def t_test(sample1, sample2):
+    t_stat, p_value = stats.ttest_ind(sample1, sample2)
+    return t_stat, p_value
+
+# Chi Square Test
+def chi_square_test(observed_data):
+    chi2, p, dof, expected = stats.chi2_contingency(observed_data)
+    return chi2, p
 
